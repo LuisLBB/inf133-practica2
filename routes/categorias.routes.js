9794f7419b9454409805083b9a156e52c34ece38
LuisLBB <luislebo97@gmail.com>
2026-06-07 10:19:07 -0400

    Update categorias.routes.js

--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // Ajusta la ruta según tu proyecto
+const db = require('../db');
 
 router.post('/', async (req, res) => {
   try {
