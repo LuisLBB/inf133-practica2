87c9a5850034bbd3afaedd9ef152b834f39ccd59
LuisLBB <luislebo97@gmail.com>
2026-06-07 10:19:55 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors    = require('cors');
+const app     = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/categorias', require('./routes/categorias.routes'));
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
